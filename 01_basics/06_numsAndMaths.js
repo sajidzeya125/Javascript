17dacda94f75e7dfec0fbd36733283ca25dfb91e
sajidzeya125 <sajidzeya294@gmail.com>
2025-05-03 12:48:09 +0000

    Num and Math

---
+++ 01_basics/06_numsAndMaths.js
@@ -0,0 +1,47 @@
+const score=100
+// console.log(score);
+
+
+const balance=new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+
+
+const number1=1123.977687
+// console.log(number1.toPrecision(4));
+
+const n1=1000000
+// console.log(n1.toLocaleString('en-IN'));
+
+
+//+++++++++++++++++++++ Maths ++++++++++++++++++++++
+
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.3));
+
+console.log(Math.min(1,2,3,4,5,6,7,8,9));
+console.log(Math.max(1,2,3,4,5,6,7,8));
+
+
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)//To get any random between a specific range 
+
+
+
+
+
+
