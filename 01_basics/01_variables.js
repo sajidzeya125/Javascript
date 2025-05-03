d3a27ade9576fe63b786902fbd3493acbc664c6d
sajidzeya125 <sajidzeya294@gmail.com>
2025-05-03 07:36:18 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail="abs@gmail.com"
+var accountPassword="12345"
+accountCity="Kanpur"
+let accountState;
+
+
+// accountId=2//not allowed
+accountEmail="jhvgjh@gmail.com"
+accountPassword="643333"
+accountCity="Jaunpur"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello")//we are using nodejs, not browser
+
+
+let name="Sajid"
+let age=20
+let isLoggedIn=false
+let state;
+
+
+//number=> 2^53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=> standalone value
+//undefined=>
+//symbol=>unique
+
+
+
+
+//object
+
+
+console.log(typeof "Sajid");
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
+
+
