ff4e422d6ca05bda562d91a056ed4d43d16afad8
sajidzeya125 <sajidzeya294@gmail.com>
2025-05-03 08:25:12 +0000

    operation

--- 01_basics/conversionOrOperation.js
+++ 01_basics/conversionOrOperation.js
@@ -29,6 +29,49 @@ let str=String(a)
 console.log(str);
 console.log(typeof str);
 
+//      OPERATIONS
+
+
+let value=3
+let negValue=-value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+
+
+// let str1="hello"
+// let str2=" yello"
+// let str3=str1+str2
+// console.log(str3);
+
+
+// console.log("1"+2+2);
+// console.log(1+"2");
+
+
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+
+
+let num1,num2,num3
+num1=num2=num3=3+3
+
+let counter=100
+counter++
+console.log(counter);
+
+
+
+
+
+
 
 
 
