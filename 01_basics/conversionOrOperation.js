fb6bdbf849aa612ba93d2ba53c9d0afed3ec5536
sajidzeya125 <sajidzeya294@gmail.com>
2025-05-03 07:53:53 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOrOperation.js
@@ -0,0 +1,36 @@
+let score=undefined
+
+console.log(typeof score);
+console.log(typeof score);
+
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;false=>0
+//null=>0
+//undefinded=>NaN
+
+let isLoggedIn=""
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+//1=>true;0=>false
+//""=>false
+//"abs"=>true
+
+let a=33
+let str=String(a)
+console.log(str);
+console.log(typeof str);
+
+
+
+
+
+
